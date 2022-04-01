3efc2789e1c3ad0b87db081a593f02ad6d8ace1b
Beatriz <beatriz.lagoperez102@gmail.com>
2022-04-02 00:33:23 +0200

    Added main.js with country pots created

---
+++ main.js
@@ -0,0 +1,172 @@
+const pot1 = [
+    {
+        country: 'Qatar',
+        region: 'AFC',
+        ranking: 0 // 0 because it's the host
+    }, 
+    {
+        country: 'Brazil',
+        region: 'CONMEBOL',
+        ranking: 1
+    },
+    {
+        country: 'Belgium',
+        region: 'UEFA',
+        ranking: 2
+    },
+    {
+        country: 'France',
+        region: 'UEFA',
+        ranking: 3
+    },
+    {
+        country: 'Argentina',
+        region: 'CONMEBOL',
+        ranking: 4
+    },
+    {
+        country: 'England',
+        region: 'UEFA',
+        ranking: 5
+    },
+    {
+        country: 'Spain',
+        region: 'UEFA',
+        ranking: 7
+    },
+    {
+        country: 'Portugal',
+        region: 'UEFA',
+        ranking: 8
+    }
+]
+const pot2 = [
+    {
+        country: 'Mexico',
+        region: 'AFC',
+        ranking: 9
+    }, 
+    {
+        country: 'Netherlands',
+        region: 'UEFA',
+        ranking: 10
+    },
+    {
+        country: 'Denmark',
+        region: 'UEFA',
+        ranking: 11
+    },
+    {
+        country: 'Germany',
+        region: 'UEFA',
+        ranking: 12
+    },
+    {
+        country: 'Uruguay',
+        region: 'CONMEBOL',
+        ranking: 13
+    },
+    {
+        country: 'Switzerland',
+        region: 'UEFA',
+        ranking: 14
+    },
+    {
+        country: 'USA',
+        region: 'UEFA',
+        ranking: 15
+    },
+    {
+        country: 'Croatia',
+        region: 'UEFA',
+        ranking: 16
+    }
+]
+const pot3 = [
+    {
+        country: 'Senegal',
+        region: 'CAF',
+        ranking: 20
+    }, 
+    {
+        country: 'Iran',
+        region: 'AFC',
+        ranking: 21
+    },
+    {
+        country: 'Japan',
+        region: 'AFC',
+        ranking: 23
+    },
+    {
+        country: 'Morocco',
+        region: 'CAF',
+        ranking: 24
+    },
+    {
+        country: 'Serbia',
+        region: 'UEFA',
+        ranking: 25
+    },
+    {
+        country: 'Poland',
+        region: 'UEFA',
+        ranking: 26
+    },
+    {
+        country: 'South Korea',
+        region: 'AFC',
+        ranking: 29
+    },
+    {
+        country: 'Tunisia',
+        region: 'CAF',
+        ranking: 35
+    }
+]
+const pot4 = [
+    {
+        country: 'Cameroon ',
+        region: 'CAF',
+        ranking: 37
+    }, 
+    {
+        country: 'Canada',
+        region: 'AFC',
+        ranking: 38
+    },
+    {
+        country: 'Ecuador',
+        region: 'AFC',
+        ranking: 46
+    },
+    {
+        country: 'Saudi Arabia',
+        region: 'CAF',
+        ranking: 49
+    },
+    {
+        country: 'Ghana',
+        region: 'UEFA',
+        ranking: 60
+    },
+    {
+        country: 'Playoff1',
+        region: 'UEFA',
+        ranking: -1
+    },
+    {
+        country: 'Playoff2',
+        region: 'AFC',
+        ranking: -1
+    },
+    {
+        country: 'EuroPlayoff',
+        region: 'UEFA',
+        ranking: -1
+    }
+]
+
+function drawTeams(){
+    
+}
